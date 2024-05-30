9a0db8ce8c1bdea51dffd567e5f1a4ae08d1be18
offhub <6871698+offhub@users.noreply.github.com>
2024-05-31 02:10:59 +0300

    Update install_qt.cmd

--- SandboxiePlus/install_qt.cmd
+++ SandboxiePlus/install_qt.cmd
@@ -1,16 +1,19 @@
+set "qt_version=%qt_version%"
+set "ghUser=%ghUser%"
+set "ghRepo=%ghRepo%"
 if %1 == Win32 (
-    if exist %~dp0..\..\Qt\5.15.14\msvc2019\bin\qmake.exe goto done
+    if exist %~dp0..\..\Qt\%qt_version%\msvc2019\bin\qmake.exe goto done
 
-    curl -LsSO --output-dir %~dp0..\..\ https://github.com/offhub/qt-sbie-builds/releases/download/v5.15.14-ssl-lgpl/qt-everywhere-5.15.14-Windows_10-MSVC2019-x86.7z
-    "C:\Program Files\7-Zip\7z.exe" x -aoa -o%~dp0..\..\Qt\ %~dp0..\..\qt-everywhere-5.15.14-Windows_10-MSVC2019-x86.7z
-    certutil -hashfile %~dp0..\..\qt-everywhere-5.15.14-Windows_10-MSVC2019-x86.7z SHA256 | find /i "1E22E6D81AF478FD52AE5F0F465523CE042325127556B57E6731802D03C04622"
+    curl -LsSO --output-dir %~dp0..\..\ https://github.com/%ghUser%/%ghRepo%/releases/download/v%qt_version%-ssl-lgpl/qt-everywhere-%qt_version%-Windows_10-MSVC2019-x86.7z
+    "C:\Program Files\7-Zip\7z.exe" x -aoa -o%~dp0..\..\Qt\ %~dp0..\..\qt-everywhere-%qt_version%-Windows_10-MSVC2019-x86.7z
+    certutil -hashfile %~dp0..\..\qt-everywhere-%qt_version%-Windows_10-MSVC2019-x86.7z SHA256 | find /i "1E22E6D81AF478FD52AE5F0F465523CE042325127556B57E6731802D03C04622"
 )
 if %1 == x64 (
-    if exist %~dp0..\..\Qt\5.15.14\msvc2019_64\bin\qmake.exe goto done
+    if exist %~dp0..\..\Qt\%qt_version%\msvc2019_64\bin\qmake.exe goto done
 
-    curl -LsSO --output-dir %~dp0..\..\ https://github.com/offhub/qt-sbie-builds/releases/download/v5.15.14-ssl-lgpl/qt-everywhere-5.15.14-Windows_10-MSVC2019-x86_64.7z
-    "C:\Program Files\7-Zip\7z.exe" x -aoa -o%~dp0..\..\Qt\ %~dp0..\..\qt-everywhere-5.15.14-Windows_10-MSVC2019-x86_64.7z
-    certutil -hashfile %~dp0..\..\qt-everywhere-5.15.14-Windows_10-MSVC2019-x86_64.7z SHA256 | find /i "5764FE462D99ADA5871E9D87DCBEA36D40733978C9F9E91E2F9F4D921412DC3B"
+    curl -LsSO --output-dir %~dp0..\..\ https://github.com/%ghUser%/%ghRepo%/releases/download/v%qt_version%-ssl-lgpl/qt-everywhere-%qt_version%-Windows_10-MSVC2019-x86_64.7z
+    "C:\Program Files\7-Zip\7z.exe" x -aoa -o%~dp0..\..\Qt\ %~dp0..\..\qt-everywhere-%qt_version%-Windows_10-MSVC2019-x86_64.7z
+    certutil -hashfile %~dp0..\..\qt-everywhere-%qt_version%-Windows_10-MSVC2019-x86_64.7z SHA256 | find /i "5764FE462D99ADA5871E9D87DCBEA36D40733978C9F9E91E2F9F4D921412DC3B"
 )
 
 if %ERRORLEVEL% == 1 exit /b 1
@@ -19,4 +22,4 @@ if %ERRORLEVEL% == 1 exit /b 1
 
 REM dir %~dp0..\..\
 REM dir %~dp0..\..\Qt
-REM dir %~dp0..\..\Qt\5.15.14
+REM dir %~dp0..\..\Qt\%qt_version%
