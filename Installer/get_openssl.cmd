8b31c3590b7038ab35a6f3555206706e91d10c80
offhub <6871698+offhub@users.noreply.github.com>
2024-05-30 23:18:35 +0300

    Qt-5.15.14-OpenSSL-1.1.1w

--- Installer/get_openssl.cmd
+++ Installer/get_openssl.cmd
@@ -1,6 +1,17 @@
-set version=1_1_1p
+echo %*
+IF "%~3" == "" ( set "ghSsl_repo=openssl" ) ELSE ( set "ghSsl_repo=%~3" )
+IF "%~2" == "" ( set "ghSsl_user=DavidXanatos" ) ELSE ( set "ghSsl_user=%~2" )
+IF "%~1" == "" ( set "openssl_version=3.3.0" ) ELSE ( set "openssl_version=%~1" )
+
+IF "%openssl_version:~0,1%" == "1" set "openssl_version_underscore=%openssl_version:.=_%"
 
 mkdir %~dp0\OpenSSL
-curl -L --url https://github.com/DavidXanatos/openssl/releases/download/OpenSSL_%version%/OpenSSL-%version%.zip -o %~dp0\OpenSSL\OpenSSL-%version%.zip --ssl-no-revoke
-"C:\Program Files\7-Zip\7z.exe" x -bd -o%~dp0\OpenSSL\ %~dp0\OpenSSL\OpenSSL-%version%.zip
+
+IF "%openssl_version:~0,1%" == "1" (
+curl -L --url https://github.com/%ghSsl_user%/%ghSsl_repo%/releases/download/OpenSSL_%openssl_version_underscore%/OpenSSL-%openssl_version_underscore%.zip -o %~dp0\OpenSSL\OpenSSL-%openssl_version%.zip --ssl-no-revoke
+) ELSE (
+curl -L --url https://github.com/%ghSsl_user%/%ghSsl_repo%/releases/download/openssl-%openssl_version%/openssl-%openssl_version%.zip -o %~dp0\OpenSSL\OpenSSL-%openssl_version%.zip --ssl-no-revoke
+)
+
+"C:\Program Files\7-Zip\7z.exe" x -bd -o%~dp0\OpenSSL\ %~dp0\OpenSSL\OpenSSL-%openssl_version%.zip
 
