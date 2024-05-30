180a20108315c62f39ae6460c019bf612d9c4086
offhub <6871698+offhub@users.noreply.github.com>
2024-05-31 02:19:21 +0300

    Update get_openssl.cmd

--- Installer/get_openssl.cmd
+++ Installer/get_openssl.cmd
@@ -1,6 +1,8 @@
-set version=3.3.0
+set "version=3.3.0"
+set "ghUser=offhub"
+set "ghRepo=openssl"
 
 mkdir %~dp0\OpenSSL
-curl -L --url https://github.com/offhub/openssl/releases/download/OpenSSL_%version%/OpenSSL-%version%.zip -o %~dp0\OpenSSL\OpenSSL-%version%.zip --ssl-no-revoke
+curl -L --url https://github.com/%ghUser%/%ghRepo%/releases/download/OpenSSL_%version%/OpenSSL-%version%.zip -o %~dp0\OpenSSL\OpenSSL-%version%.zip --ssl-no-revoke
 "C:\Program Files\7-Zip\7z.exe" x -bd -o%~dp0\OpenSSL\ %~dp0\OpenSSL\OpenSSL-%version%.zip
 
