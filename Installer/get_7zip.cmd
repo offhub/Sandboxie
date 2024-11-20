99b8e3b5ae222e14ce24de7c985be0bd09ffd96e
offhub <6871698+offhub@users.noreply.github.com>
2024-11-20 11:07:57 +0300

    Update get_7zip.cmd

--- Installer/get_7zip.cmd
+++ Installer/get_7zip.cmd
@@ -1,3 +1,5 @@
+@echo on
+
 mkdir %~dp0\7-Zip
 curl -L --url https://github.com/DavidXanatos/7z/releases/download/23.01/7z2301.zip -o %~dp0\7-Zip\7z2301.zip --ssl-no-revoke
 "C:\Program Files\7-Zip\7z.exe" x -bd -o%~dp0\7-Zip\ %~dp0\7-Zip\7z2301.zip
