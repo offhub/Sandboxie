08c386d15ad52803d95111f3021ea1d1dda04b76
offhub <6871698+offhub@users.noreply.github.com>
2024-11-25 18:46:29 +0300

    Update buildVariables.cmd

--- Installer/buildVariables.cmd
+++ Installer/buildVariables.cmd
@@ -10,7 +10,7 @@ set "ghQtBuilds_repo=qt-builds"
 set "ghQtBuilds_hash_x86=502e9a36a52918af4e116cd74c16c6c260d029087aaeee3775ab0e5d3f6a2705"
 set "ghQtBuilds_hash_x64=673c288feeabd11ec66f9f454d49cde3945cbd3e3f71283b7a6c4df0893b19f2"
 set "msvc_version=2019"
-set "vctools_version=143"
+set "vctools_version=142"
 
 REM catch build_qt6
 set "allArgs=%*"
