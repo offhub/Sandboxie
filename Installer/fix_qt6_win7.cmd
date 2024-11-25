9f55f0289657e6451877e1bd9f7bddbdd84f3345
offhub <6871698+offhub@users.noreply.github.com>
2024-11-25 18:39:38 +0300

    Update fix_qt6_win7.cmd

--- Installer/fix_qt6_win7.cmd
+++ Installer/fix_qt6_win7.cmd
@@ -6,4 +6,4 @@ REM IF "%~2" == "" ( set "ghQt6Win7_user=DavidXanatos" ) ELSE ( set "ghQt6Win7_u
 REM IF "%~1" == "" ( set "qt6_version=6.3.1" ) ELSE ( set "qt6_version=%~1" )
 
 curl --ssl-no-revoke -L https://github.com/%ghQt6Win7_user%/%ghQt6Win7_repo%/releases/download/v%qt6_version%-w7/Qt%qt6_version%-w7.zip -o %~dp0..\..\Qt%qt6_version%-w7.zip
-"C:\Program Files\7-Zip\7z.exe" x -o%~dp0..\..\Qt\%qt6_version%\msvc2019_64\ %~dp0..\..\Qt%qt6_version%-w7.zip -y
+"C:\Program Files\7-Zip\7z.exe" x -o%~dp0..\..\Qt\%qt6_version%\msvc%msvc_version%_64\ %~dp0..\..\Qt%qt6_version%-w7.zip -y
