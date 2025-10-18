e041923b45bc890a1110eac184e51f9a8c02c14a
offhub <6871698+offhub@users.noreply.github.com>
2025-10-18 12:22:26 +0300

    Update OpenSSL version and GitHub user/repo

--- Installer/buildVariables.cmd
+++ Installer/buildVariables.cmd
@@ -2,9 +2,9 @@
 
 set "qt_version=6.10.0"
 set "qt6_version=6.10.0"
-set "openssl_version=3.4.0"
-set "ghSsl_user=xanasoft"
-set "ghSsl_repo=openssl-builds"
+set "openssl_version=3.6.0"
+set "ghSsl_user=offhub"
+set "ghSsl_repo=openssl"
 set "ghQtBuilds_user=xanasoft"
 set "ghQtBuilds_repo=qt-builds"
 rem set "ghQtBuilds_hash_x86=6b25cb24755b158e580e73b7889a659e093012cc3d6428d2bfbff8ebad238cf2"
