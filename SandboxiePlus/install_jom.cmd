508ee2a8eb642ce6a71bb99896febb45063e7952
offhub <6871698+offhub@users.noreply.github.com>
2026-08-15 11:50:04 +0300

    Update install_jom.cmd to download and extract jom

--- SandboxiePlus/install_jom.cmd
+++ SandboxiePlus/install_jom.cmd
@@ -1,10 +1,27 @@
-if exist %~dp0..\..\Qt\Tools\QtCreator\bin\jom.exe goto done
+set "JOM_BIN=%~dp0..\..\Qt\Tools\QtCreator\bin\jom.exe"
+set "JOM_ZIP=%~dp0..\..\jom_1_1_4.zip"
+set "JOM_URL=https://download.qt.io/official_releases/jom/jom_1_1_4.zip"
 
-curl -LsSO --output-dir %~dp0..\..\ https://download.qt.io/official_releases/jom/jom_1_1_4.zip
-"C:\Program Files\7-Zip\7z.exe" x -aoa -o%~dp0..\..\Qt\Tools\QtCreator\bin\ %~dp0..\..\jom_1_1_4.zip
+if exist "%JOM_BIN%" goto done
+
+if not exist "%JOM_ZIP%" (
+    curl -LsSO --output-dir %~dp0..\..\ %JOM_URL%
+    if ERRORLEVEL 1 goto failed
+)
+
+"C:\Program Files\7-Zip\7z.exe" x -aoa -o%~dp0..\..\Qt\Tools\QtCreator\bin\ "%JOM_ZIP%"
+if ERRORLEVEL 1 goto failed
+
+if not exist "%JOM_BIN%" goto failed
 
 :done
 
 REM dir %~dp0..\..\
 REM dir %~dp0..\..\Qt
 REM dir %~dp0..\..\Qt\Tools
+
+goto :eof
+
+:failed
+echo Failed to install jom.exe
+exit /b 1
