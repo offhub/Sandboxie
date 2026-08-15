ba70d2c403d9db2c533d4081b74a70596d452352
offhub <6871698+offhub@users.noreply.github.com>
2026-08-15 13:27:23 +0300

    Simplify jom installation script
    Removed error check for missing jom.exe after extraction.

--- SandboxiePlus/install_jom.cmd
+++ SandboxiePlus/install_jom.cmd
@@ -2,6 +2,7 @@
 setlocal
 
 set "ROOT=%~dp0..\..\"
+for %%I in ("%ROOT%") do set "ROOT=%%~fI"
 set "ZIP=%ROOT%jom_1_1_4.zip"
 set "JOM_EXE=%ROOT%Qt\Tools\QtCreator\bin\jom.exe"
 
@@ -13,11 +14,6 @@ if not exist "%ZIP%" (
 
 if not exist "%ROOT%Qt\Tools\QtCreator\bin\" mkdir "%ROOT%Qt\Tools\QtCreator\bin" || exit /b 1
 "C:\Program Files\7-Zip\7z.exe" x -aoa -o"%ROOT%Qt\Tools\QtCreator\bin\" "%ZIP%" || exit /b 1
-
-if not exist "%JOM_EXE%" (
-  echo ERROR: jom.exe missing after extraction
-  exit /b 1
-)
 
 :done
 endlocal
