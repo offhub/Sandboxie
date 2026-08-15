72d6366f9d64e46f2a23ad2f762c64c05222a942
offhub <6871698+offhub@users.noreply.github.com>
2026-08-15 12:21:52 +0300

    Fix paths and download command in install_jom.cmd

--- SandboxiePlus/install_jom.cmd
+++ SandboxiePlus/install_jom.cmd
@@ -1,15 +1,19 @@
-set "JOM_BIN=%~dp0..\..\Qt\Tools\QtCreator\bin\jom.exe"
-set "JOM_ZIP=%~dp0..\..\jom_1_1_4.zip"
+@echo off
+setlocal
+
+set "ROOT=%~dp0..\.."
+set "JOM_BIN=%ROOT%\Qt\Tools\QtCreator\bin\jom.exe"
+set "JOM_ZIP=%ROOT%\.jom_1_1_4.zip"
 set "JOM_URL=https://download.qt.io/official_releases/jom/jom_1_1_4.zip"
 
 if exist "%JOM_BIN%" goto done
 
 if not exist "%JOM_ZIP%" (
-    curl -LsSO --output-dir %~dp0..\..\ %JOM_URL%
+    curl -LsS --fail -o "%JOM_ZIP%" "%JOM_URL%"
     if ERRORLEVEL 1 goto failed
 )
 
-"C:\Program Files\7-Zip\7z.exe" x -aoa -o%~dp0..\..\Qt\Tools\QtCreator\bin\ "%JOM_ZIP%"
+"C:\Program Files\7-Zip\7z.exe" x -aoa -o"%ROOT%\Qt\Tools\QtCreator\bin\" "%JOM_ZIP%"
 if ERRORLEVEL 1 goto failed
 
 if not exist "%JOM_BIN%" goto failed
